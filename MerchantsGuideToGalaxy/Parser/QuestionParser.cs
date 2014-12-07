6a24d5f4bbb59ce728ffefde92da786abbc02f6f
akshaymuley <akshaymuley@gmail.com>
2014-12-07 22:28:04 +0530

    Corrected output format

--- MerchantsGuideToGalaxy/Parser/QuestionParser.cs
+++ MerchantsGuideToGalaxy/Parser/QuestionParser.cs
@@ -8,6 +8,12 @@ namespace MerchantsGuideToGalaxy
 {
     public class QuestionParser : StringParser
     {
+        private const string goodsValueOutputFormat = "{0} is {1} Credits";
+        
+        private const string valueConversion = "{0} is {1}";
+        
+        private List<string> values;
+
         public QuestionParser(IGalaxy galaxy)
             : base(galaxy)
         {
@@ -21,6 +27,7 @@ namespace MerchantsGuideToGalaxy
         public override void Parse(string stringToParse)
         {
             stringToParse = stringToParse.Trim();
+            values = new List<string>();
 
             if (!IsValidString(stringToParse))
             {
@@ -31,6 +38,7 @@ namespace MerchantsGuideToGalaxy
             var romanValue = string.Empty;
             string productName = string.Empty;
             var words = stringToParse.Trim().Split(' ');
+
             for (int i = 0; i < words.Length; i++)
             {
                 if (string.IsNullOrEmpty(romanValue))
@@ -39,13 +47,15 @@ namespace MerchantsGuideToGalaxy
                     if (string.IsNullOrEmpty(valueString))
                         continue;
                     romanValue = string.Concat(romanValue, valueString);
-                    
+                    values.Add(words[i]);
+
                     for (; i < words.Length; )
                     {
                         valueString = galaxy.GetRomanValue(words[i + 1]);
                         if (string.IsNullOrEmpty(valueString))
                             break;
                         romanValue = string.Concat(romanValue, valueString);
+                        values.Add(words[i+1]);
                         i++;
                     }
                 }
@@ -60,15 +70,15 @@ namespace MerchantsGuideToGalaxy
             }
 
             if (string.IsNullOrEmpty(romanValue) || string.IsNullOrEmpty(productName))
-                Console.WriteLine("I have no idea what you are talking about");
+                Console.WriteLine("I have no idea what you are talking about.");
             else
             {
                 var productValue = galaxy.GetProductsValue(productName);
                 var quantity = RomanValueConverter.GetValue(romanValue);
                 if (productValue != 0.0 && quantity != 0)
-                    Console.WriteLine(productValue * quantity);
+                    PrintOutput(productValue * quantity , false);
                 else if (quantity != 0)
-                    Console.WriteLine(quantity);
+                    PrintOutput(quantity, true);
             }
         }
 
@@ -77,6 +87,16 @@ namespace MerchantsGuideToGalaxy
             if (stringToParse[stringToParse.Trim().Length - 1].Equals('?'))
                 return true;
             return false;
+        }
+
+        private void PrintOutput(float credits, bool isValueConversionString)
+        {
+            var valueString = string.Empty;
+            foreach (string value in values)
+            {
+                valueString = string.Format("{0} {1}", valueString, value);
+            }
+            Console.WriteLine(string.Format(isValueConversionString ? valueConversion: goodsValueOutputFormat, valueString.Trim(), credits));
         }
     }
 }
